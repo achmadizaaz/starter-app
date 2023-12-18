019ce6004bf367375c106b338802860a3bec4ea8
AchmadIzaaz <myachmadizaaz@gmail.com>
2023-12-18 21:02:18 +0700

    Add: SweetAlert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,8 @@
+// Bootstrap 5
 import "./bootstrap";
-import "./bootstrap.bundle.min";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+
+// import "sweetalert2/src/sweetalert2.js";
+// // SweetAlert 2
+import swal from "sweetalert2";
+window.Swal = swal;
